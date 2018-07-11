8d422b540536218ef2cf6c3de04230f6463ec7b1
Pawel Bednarz <pbedn@users.noreply.github.com>
2018-07-11 21:31:04 +0200

    feat: Move the paddle (two players)

--- games/pong/game.py
+++ games/pong/game.py
@@ -1,6 +1,7 @@
 import random
 
 import pyglet
+from pyglet.window import key
 from pyglet.gl import *
 
 
@@ -15,16 +16,20 @@ WINDOW_HEIGHT_HALF = window.height // 2
 
 fps_display = pyglet.clock.ClockDisplay()
 
+keys = key.KeyStateHandler()
+window.push_handlers(keys)
+
 
 def init():
     global paddle_left_x, paddle_left_y, paddle_right_x, paddle_right_y
     global ball_pos_x, ball_pos_y, ball_dir_x, ball_dir_y
-    global paddle_width, paddle_height
+    global paddle_width, paddle_height, paddle_speed
     global ball_size, ball_speed
 
     # paddles in general
     paddle_width = window.width // 57
     paddle_height = window.height // 8
+    paddle_speed = 9
 
     # left paddle position
     paddle_left_x = window.width // 10
@@ -79,17 +84,51 @@ def on_draw():
 
 def update(dt):
     global ball_pos_x, ball_pos_y, ball_dir_x, ball_dir_y
-    global ball_speed
+    global paddle_left_x, paddle_left_y, paddle_right_x, paddle_right_y
+    global paddle_speed, ball_speed, ball_size
+    global paddle_width, paddle_height
 
-    # fly a bit
+    if keys[key.W]:
+        paddle_left_y += paddle_speed
+    if keys[key.S]:
+        paddle_left_y -= paddle_speed
+
+    if keys[key.UP]:
+        paddle_right_y += paddle_speed
+    if keys[key.DOWN]:
+        paddle_right_y -= paddle_speed
+
+    # fly the ball
     ball_pos_x += ball_dir_x * ball_speed
     ball_pos_y += ball_dir_y * ball_speed
+
+    # check for collisions of ball with left paddle
+    if (paddle_left_x < ball_pos_x < paddle_left_x + paddle_width and
+       paddle_left_y < ball_pos_y < paddle_left_y + paddle_height):
+        # set fly direction depending on where it hit the paddle
+        # (t is 0.5 if hit at top, 0 at center, -0.5 at bottom)
+        t = ((ball_pos_y - paddle_left_y) / paddle_height) - 0.5
+        ball_dir_x = abs(ball_dir_x)  # force it to be positive
+        ball_dir_y = t
+
+        ball_speed += 0.5
+
+    # check for collisions of ball with right paddle
+    if (paddle_right_x < ball_pos_x < paddle_right_x + paddle_width and
+       paddle_right_y < ball_pos_y < paddle_right_y + paddle_height):
+        # set fly direction depending on where it hit the paddle
+        # (t is 0.5 if hit at top, 0 at center, -0.5 at bottom)
+        t = ((ball_pos_y - paddle_right_y) / paddle_height) - 0.5
+        ball_dir_x = -abs(ball_dir_x)  # force it to be negative
+        ball_dir_y = t
+
+        ball_speed += 0.5
 
     # check for left wall collision
     if ball_pos_x < 0:
         ball_pos_x = window.width / 2
         ball_pos_y = window.height / 2
-        ball_dir_x = ball_dir_x  # force it to be positive
+        ball_dir_x = abs(ball_dir_x)  # force it to be positive
         ball_dir_y = 0
         init()
 
@@ -97,17 +136,17 @@ def update(dt):
     if ball_pos_x > window.width:
         ball_pos_x = window.width / 2
         ball_pos_y = window.height / 2
-        ball_dir_x = -ball_dir_x  # force it to be negative
+        ball_dir_x = -abs(ball_dir_x)  # force it to be negative
         ball_dir_y = 0
         init()
 
     # check for top wall collision
     if ball_pos_y > window.height:
-        ball_dir_y = -ball_dir_y  # force it to be negative
+        ball_dir_y = -abs(ball_dir_y)  # force it to be negative
 
     # check for bottom wall collision
     if ball_pos_y < 0:
-        ball_dir_y = abs(ball_dir_y) # force it to be positive
+        ball_dir_y = abs(ball_dir_y)  # force it to be positive
 
 
 if __name__ == '__main__':
